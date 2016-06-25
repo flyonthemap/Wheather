21f65060854076e04a68269280f250ddba6944e6
flyonthemap <18710892958@163.com>
2016-06-25 20:58:26 +0800

    增加程序的数据配置信息，便于统一管理

---
+++ app/src/main/java/com/flyonthemap/wheather/Config.java
@@ -0,0 +1,32 @@
+package com.flyonthemap.wheather;
+
+/**
+ * Created by flyonthemap on 16/6/24.
+ */
+public class Config {
+    public static final String CITY_NAME_URL = "http://v.juhe.cn/weather/index?cityname=";
+    public static final String RESULTCODE = "resultcode";
+    public static final String SUCCESS_RESULT_CODE = "200";
+    public static final String TODAY = "today";
+    public static final String CURRENT_WEATHER = "sk";
+    public static final String TEMPERATURE = "temperature";
+    public static final String RESULT = "result";
+    public static final String WEATHER_KEY = "e8d9eaed51eae76a05ade7ff51920efa";
+    public static final String UV_INDEX = "uv_index";
+    public static final String DRESSING_INDEX = "dressing_index";
+    public static final String CITY = "city";
+    public static final String WIND_DIRECTION = "wind_direction";
+    public static final String WIND_STRENGTH = "wind_strength";
+    public static final String HUMIDITY = "humidity";
+    public static final String RELEASE = "release";
+    public static final String WEATHER_ID = "weather_id";
+    public static final String FA = "fa";
+    public static final String WEATHER = "weather";
+    public static final String TEMPERATURE_CURRENT = "temp";
+    public static final String FUTURE_WEATHER = "future";
+    public static final String DATE = "date";
+    public static final String WEEK = "week";
+    public static final String TAG = "DEBUG";
+//    这是Debug实收使用的数据
+    public static final String TEST = "{\"resultcode\":\"200\",\"reason\":\"successed!\",\"result\":{\"sk\":{\"temp\":\"21\",\"wind_direction\":\"西南风\",\"wind_strength\":\"1级\",\"humidity\":\"93%\",\"time\":\"15:39\"},\"today\":{\"temperature\":\"19℃~23℃\",\"weather\":\"小雨转多云\",\"weather_id\":{\"fa\":\"07\",\"fb\":\"01\"},\"wind\":\"南风微风\",\"week\":\"星期六\",\"city\":\"上海\",\"date_y\":\"2016年06月25日\",\"dressing_index\":\"较舒适\",\"dressing_advice\":\"建议着薄外套、开衫牛仔衫裤等服装。年老体弱者应适当添加衣物，宜着夹克衫、薄毛衣等。\",\"uv_index\":\"最弱\",\"comfort_index\":\"\",\"wash_index\":\"不宜\",\"travel_index\":\"较不宜\",\"exercise_index\":\"较不宜\",\"drying_index\":\"\"},\"future\":{\"day_20160625\":{\"temperature\":\"19℃~23℃\",\"weather\":\"小雨转多云\",\"weather_id\":{\"fa\":\"07\",\"fb\":\"01\"},\"wind\":\"南风微风\",\"week\":\"星期六\",\"date\":\"20160625\"},\"day_20160626\":{\"temperature\":\"21℃~26℃\",\"weather\":\"阴转阵雨\",\"weather_id\":{\"fa\":\"02\",\"fb\":\"03\"},\"wind\":\"东南风微风\",\"week\":\"星期日\",\"date\":\"20160626\"},\"day_20160627\":{\"temperature\":\"22℃~25℃\",\"weather\":\"中雨\",\"weather_id\":{\"fa\":\"08\",\"fb\":\"08\"},\"wind\":\"东南风微风\",\"week\":\"星期一\",\"date\":\"20160627\"},\"day_20160628\":{\"temperature\":\"24℃~30℃\",\"weather\":\"雷阵雨\",\"weather_id\":{\"fa\":\"04\",\"fb\":\"04\"},\"wind\":\"南风微风\",\"week\":\"星期二\",\"date\":\"20160628\"},\"day_20160629\":{\"temperature\":\"25℃~29℃\",\"weather\":\"雷阵雨转中雨\",\"weather_id\":{\"fa\":\"04\",\"fb\":\"08\"},\"wind\":\"西南风微风\",\"week\":\"星期三\",\"date\":\"20160629\"},\"day_20160630\":{\"temperature\":\"24℃~30℃\",\"weather\":\"雷阵雨\",\"weather_id\":{\"fa\":\"04\",\"fb\":\"04\"},\"wind\":\"南风微风\",\"week\":\"星期四\",\"date\":\"20160630\"},\"day_20160701\":{\"temperature\":\"24℃~30℃\",\"weather\":\"雷阵雨\",\"weather_id\":{\"fa\":\"04\",\"fb\":\"04\"},\"wind\":\"南风微风\",\"week\":\"星期五\",\"date\":\"20160701\"}}},\"error_code\":0}";
+}
